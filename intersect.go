cbe6bf39410fd0101866f11669be18e07bf3207a
Athreya S Shreyas <shreyas.athreya_s@nokia.com>
2018-02-23 14:25:14 +0530

    added nil check

--- intersect.go
+++ intersect.go
@@ -8,6 +8,9 @@ import (
 // Complexity: O(n^2)
 func Simple(a interface{}, b interface{}) interface{} {
 	set := make([]interface{}, 0)
+	if a == nil || b == nil {
+		return set
+	}
 	av := reflect.ValueOf(a)
 
 	for i := 0; i < av.Len(); i++ {
@@ -23,6 +26,9 @@ func Simple(a interface{}, b interface{}) interface{} {
 // Complexity: O(n * log(n)), a needs to be sorted
 func Sorted(a interface{}, b interface{}) interface{} {
 	set := make([]interface{}, 0)
+	if a == nil || b == nil {
+		return set
+	}
 	av := reflect.ValueOf(a)
 	bv := reflect.ValueOf(b)
 
@@ -42,6 +48,9 @@ func Sorted(a interface{}, b interface{}) interface{} {
 // Complexity: O(n * x) where x is a factor of hash function efficiency (between 1 and 2)
 func Hash(a interface{}, b interface{}) interface{} {
 	set := make([]interface{}, 0)
+	if a == nil || b == nil {
+		return set
+	}
 	hash := make(map[interface{}]bool)
 	av := reflect.ValueOf(a)
 	bv := reflect.ValueOf(b)
--- intersect_test.go
+++ intersect_test.go
@@ -4,14 +4,19 @@ import (
 	"testing"
 
 	"github.com/bmizerany/assert"
+	"fmt"
+	"reflect"
 )
 
-func TestSimple(t *testing.T) {
-	s := Simple([]int{1}, []int{2})
-	assert.Equal(t, s, []interface{}{})
 
-	s = Simple([]int{1, 2}, []int{2})
-	assert.Equal(t, s, []interface{}{2})
+func TestSimple(t *testing.T) {
+
+	s := Simple([]string{"a","x"}, []string{"a","b"})
+	fmt.Println(s)
+	t1 := reflect.ValueOf(s)
+	for i := 0; i < t1.Len() ; i ++ {
+		fmt.Println(t1.Index(i).Interface().(string))
+	}
 }
 
 func TestSorted(t *testing.T) {
